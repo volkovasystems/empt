b36754d1575d35a5daf6c8416578e5bdf5023587
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-03 19:02:57 +0800

    Updated test.

--- empt-test.js
+++ empt-test.js
@@ -2,6 +2,7 @@ const assert = require( "assert" );
 const empt = require( "./empt.js" );
 
 assert.equal( empt( { } ), true, "should be true" );
+
 assert.equal( empt( { "name": "simple" } ), false, "should be false" );
 
 console.log( "ok" );
